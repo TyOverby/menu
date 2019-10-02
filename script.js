210f561e1a3d9e5eab74704f8e6bf42616a2cfcf
Ty Overby <ty@pre-alpha.com>
2019-10-02 01:33:22 +0000

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function make_recipe(name, recipe) {
   let ingredients = Object.getOwnPropertyNames(recipe.ingredients).map(function (name) {
       if (name.startsWith("*") {
-          name = `<strong>${name.substr(1)}</strong>`
+          name = `<strong>${name.substr(1)}</strong>`;
       }
       return `<tr>
         <td> ${name} </td> 
