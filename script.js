4ea9f99f7327bc661fdef766e9ec1c077a48ae45
Ty Overby <ty@pre-alpha.com>
2019-10-02 01:09:41 +0000

    update

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
 
+function make_recipe(recipe) {
+  return `
+        <section>
+            <h1> Gin and Tonic </h1>
+            <div class="image"></div>
+            <div class="ingredients">
+                <table> 
+                    <tr> 
+                        <td> <strong> Gin </strong> </td> <td> 2 oz </td>
+                    </tr>
+                    <tr> 
+                        <td> Tonic </td> <td> 6 oz </td>
+                    </tr>
+                </table>
+            </div>
+            <div>
+                <div class="percent-bar">
+                    <span> sweet </span>
+                    <div style="width:20%">
+                        <span> sweet </span>
+                    </div>
+                </div>
+                <div class="percent-bar">
+                    <span> alcohol </span>
+                    <div style="width:50%">
+                        <span> alcohol </span>
+                    </div>
+                </div>
+                <div class="percent-bar">
+                    <span> sour </span>
+                    <div style="width:10%">
+                        <span> sour </span>
+                    </div>
+                </div>
+                <div class="percent-bar">
+                    <span> bitter </span>
+                    <div style="width:15%">
+                        <span> bitter </span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    `
+}
+
 setTimeout(function () {
     window.location.reload(true)
 }, 1000);
