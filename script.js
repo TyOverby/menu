9ce6f1e0acff92ddd27779fa0be6db2d2fed962b
Ty Overby <ty@pre-alpha.com>
2019-10-20 21:31:35 +0000

    update

--- script.js
+++ script.js
@@ -62,73 +62,84 @@ function make_recipe(name, recipe) {
 
 var categories = {
     "Seasonal": {
-    "Gin & Tonic": {
-        ingredients: {
-            "*Gin": "2 oz",
-            "Tonic": "6 oz",
+        "Maple Syrup Old Fashioned": {
+            ingredients: {
+                "*Burbon": "2 1/4  oz",
+                "*Rouge Vermouth": "1 oz",
+                "Maple Syrup": "1/2 oz",
+                "Angostura Bitters": "1 dash",
+            },
+            sweet: "90%",
+            alcohol: "80%",
+            sour: "0%",
+            bitter: "10%",
         },
-        sweet: "20%",
-        alcohol: "50%",
-        sour: "10%",
-        bitter: "15%",
+        "Bramble": {
+            ingredients: {
+                "*Gin": "2 oz",
+                "Lemon Juice": "1 oz",
+                "Simple Syrup": "1/3 oz",
+                "Crème de Mûre": "1/2 oz",
+            },
+            sweet: "40%",
+            alcohol: "30%",
+            sour: "70%",
+            bitter: "10%",
+        },
+        "Boulevardier": {
+            ingredients: {
+                "*Rye Whiskey": "1 oz",
+                "*Campari": "1 oz",
+                "*Sweet Vermouth": "1 oz",
+            },
+            sweet: "60%",
+            alcohol: "70%",
+            sour: "0%",
+            bitter: "90%",
+        }, "Pain Killer": {
+            ingredients: {
+                "*Dark Rum": "2 oz",
+                "Pineapple Juice": "4 oz",
+                "Cream of Coconut": "1 oz",
+                "Orange Juice": "1 oz",
+            },
+            sweet: "70%",
+            alcohol: "30%",
+            sour: "50%",
+            bitter: "0%",
+        }, "Margarita": {
+            ingredients: {
+                "*Tequila": "2 oz",
+                "*Cointreau": "1 oz",
+                "Lime Juice": "3/4 oz"
+            },
+            sweet: "70%",
+            alcohol: "80%",
+            sour: "75%",
+            bitter: "0%",
+        }
     },
-    "Maple Syrup Old Fashioned": {
-        ingredients: {
-            "*Burbon": "2 1/4  oz",
-            "*Rouge Vermouth": "1 oz",
-            "Maple Syrup": "1/2 oz",
-            "Angostura Bitters": "1 dash",
-        },
-        sweet: "90%",
-        alcohol: "80%",
-        sour: "0%",
-        bitter: "10%",
-    },
-    "Bramble": {
-        ingredients: {
-            "*Gin": "2 oz",
-            "Lemon Juice": "1 oz",
-            "Simple Syrup": "1/3 oz",
-            "Crème de Mûre": "1/2 oz",
-        },
-        sweet: "40%",
-        alcohol: "30%",
-        sour: "70%",
-        bitter: "10%",
-    },
-    "Boulevardier": {
-        ingredients: {
-            "*Rye Whiskey": "1 oz",
-            "*Campari": "1 oz",
-            "*Sweet Vermouth": "1 oz",
-        },
-        sweet: "60%",
-        alcohol: "70%",
-        sour: "0%",
-        bitter: "90%",
-    }, "Pain Killer": {
-        ingredients: {
-            "*Dark Rum": "2 oz",
-            "Pineapple Juice": "4 oz",
-            "Cream of Coconut": "1 oz",
-            "Orange Juice": "1 oz",
-        },
-        sweet: "70%",
-        alcohol: "30%",
-        sour: "50%",
-        bitter: "0%",
-    }, "Margarita": {
-        ingredients: {
-            "*Tequila": "2 oz",
-            "*Cointreau": "1 oz",
-            "Lime Juice": "3/4 oz"
-        },
-        sweet: "70%",
-        alcohol: "80%",
-        sour: "75%",
-        bitter: "0%",
-    }
-    }
+    "Classic": {
+        "Gin & Tonic": {
+            ingredients: {
+                "*Gin": "2 oz",
+                "Tonic": "6 oz",
+            },
+            sweet: "20%",
+            alcohol: "50%",
+            sour: "10%",
+            bitter: "15%",
+        }, "Margarita": {
+            ingredients: {
+                "*Tequila": "2 oz",
+                "*Cointreau": "1 oz",
+                "Lime Juice": "3/4 oz"
+            },
+            sweet: "70%",
+            alcohol: "80%",
+            sour: "75%",
+            bitter: "0%",
+        }
 };
 
 document.body.innerHTML = 
